73a21d2f81ddbfe2f15f45a045c95282ac6257be
Camila Oliveira <camila.espezio@gmail.com>
2023-06-15 18:24:13 -0300

    Inclusão tarefas Trilha 1

---
+++ Trilha 1_Tarefa 2/codigo.js
@@ -0,0 +1,20 @@
+const button = document.querySelector(".button");
+
+    function formularioPreenchido() {
+        var mensagem = "Formulário preenchido com sucesso! A receita será enviada para seu e-mail em breve.";
+        alert(mensagem);
+    }
+
+const button2 = document.querySelector(".button2");
+
+    function mensagemEnviada() {
+        var mensagem = "Sua mensagem foi enviada com sucesso! Entraremos em contato em breve.";
+        alert(mensagem);
+    }
+
+const gallery = document.querySelector(".gallery");
+
+    function alerta() {
+        var mensagem = "Se interessou por nossas peças? Aguarde novidades em breve!";
+        alert(mensagem);
+    }
